20d8a8dbcfaacec8a39f27bde5cccb3e029ec031
snakeice45 <jonas171192@gmail.com>
2022-05-03 18:26:00 -0500

    unit testing reservation class with MStest 3 scenarios

---
+++ TestNinja/UnitTestProject1/ReservationTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange  put all needed objs
+            var reservation = new Reservation();
+
+            //Act call the methods
+           var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingReservation_ReturnsTrue()
+        {
+            //Arrange 
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user}; //passing user to attribute
+
+            //Act 
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_OtherUserCancellingReservation_ReturnsFalse()
+        {
+            //Arrange 
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user }; //passing user to attribute
+
+            //Act 
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
